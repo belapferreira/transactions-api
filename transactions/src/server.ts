fc110f6e75e473a8558e88fbfcfd7bb6e59e7464
belapferreira <isabelapenhaferreira@gmail.com>
2025-09-28 09:49:03 -0300

    feat: add knex and sqlite3 for database management

--- transactions/src/server.ts
+++ transactions/src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 app.get('/', async () => {
-  return { message: 'Hello World!' }
+  const table = await knex('sqlite_schema').select('*')
+  return table
 })
 
 app
